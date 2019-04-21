8db18f5e9a8b7d023413f9b3632a5a1d71ae1e6f
Anthony Kim <anthony011@gmail.com>
2019-04-21 02:51:02 -0400

    config change

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,7 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+
+
+// export const API_BASE_URL =
+//     process.env.REACT_APP_API_BASE_URL || 'https://springeq.herokuapp.com';
+
